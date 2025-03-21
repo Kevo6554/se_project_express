9b4834e3d0e4cd6d1b1f29bc4eb8e729cd012575
kevinwitherspoon <kevofbe6@icloud.com>
2025-03-21 08:40:24 -0700

    project submit6

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const indexRoutes = require("./routes/index");
+const { NOT_FOUND } = require("./utils/errors");
 
 const { PORT = 3001 } = process.env;
 
@@ -21,6 +22,9 @@ app.use((req, res, next) => {
 });
 app.use(express.json());
 app.use("/", indexRoutes);
+app.use((req, res) => {
+  res.status(NOT_FOUND).json({ message: "Id provided was not found" });
+});
 app.listen(PORT, () => {
   // if everything works fine, the console will show which port the application is listening to
   console.log(`App listening at port ${PORT}`);
