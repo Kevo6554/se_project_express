899b3a4c41c8ab9d5fad4bbb5e7c7c70661301d7
kevinwitherspoon <kevofbe6@icloud.com>
2025-06-07 11:43:31 -0700

    project submit

--- models/clothingItem.js
+++ models/clothingItem.js
@@ -28,13 +28,11 @@ const clothingItemSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: "user",
   },
-  likes: [
-    {
-      type: [mongoose.Schema.Types.ObjectId],
-      ref: "user",
-      default: [],
-    },
-  ],
+  likes: {
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: "user" }],
+    default: [],
+  },
+
   createdAt: {
     type: Date,
     default: Date.now,
