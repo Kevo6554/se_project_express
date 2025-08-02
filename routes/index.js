3abe07c9a993c0121cf795694f5a14c5cb7c53ca
kevinwitherspoon <kevofbe6@icloud.com>
2025-08-02 09:15:37 -0700

    project submission

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,22 @@ const router = require("express").Router();
 const { NotFoundError } = require("../utils/errors");
 const itemRouter = require("./clothingItems");
 const { createUser, login } = require("../controllers/users");
+const { Joi, celebrate } = require("celebrate");
+
+const validateAuth = celebrate({
+  body: Joi.object().keus({
+    email: Joi.string().required().email().messages,
+    passwor: Joi.string().required().messages,
+  }),
+});
 
 const userRoutes = require("./users");
 
 router.use("/users", userRoutes);
 router.use("/items", itemRouter);
 
-router.post("/signin", login);
-router.post("/signup", createUser);
+router.post("/signin", validateAuth, login);
+router.post("/signup", validateAuth, createUser);
 
 router.use((req, res, next) =>
   next(new NotFoundError("Id provided not found"))
